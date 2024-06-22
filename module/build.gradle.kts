bdf12dcf445e2a99df7c6150d297da75228529f0
ThePedroo <pedroolimpioguerra@gmail.com>
2024-06-22 04:54:58 -0300

    fix: building process; add: web UI JavaScript code
    This commit both fixes the building process to include the webroot folder and adds the web UI JS code to allow it to fetch the values.

--- module/build.gradle.kts
+++ module/build.gradle.kts
@@ -86,6 +86,9 @@ androidComponents.onVariants { variant ->
         into("lib") {
             from(project(":loader").layout.buildDirectory.file("intermediates/stripped_native_libs/$variantLowered/out/lib"))
         }
+        into("webroot") {
+            from("${rootProject.projectDir}/webroot")
+        }
 
         val root = moduleDir.get()
 
@@ -157,6 +160,12 @@ androidComponents.onVariants { variant ->
                         Pair(
                             root.file("bin/zygiskd64").asFile,
                             root.file("bin/$abi64/zygiskd").asFile
+                        )
+                    )
+                    set.add(
+                        Pair(
+                            root.file("webroot/index.html").asFile,
+                            null
                         )
                     )
                     sig.initSign(privKey)
